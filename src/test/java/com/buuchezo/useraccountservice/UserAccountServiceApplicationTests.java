6a2ff94c00959028f40c00d1837ee5acf6bea602
Kennedy Buchichi Masinde <143694221+Buuchezo@users.noreply.github.com>
2026-09-04 13:45:55 +0200

    First implementations unversion files

---
+++ src/test/java/com/buuchezo/useraccountservice/UserAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buuchezo.useraccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
